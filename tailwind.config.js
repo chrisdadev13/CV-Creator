033fdb0a10b32d15df8a5e94a73a110e2d4ca268
chrisdadev13 <chrisdadev13@gmail.com>
2022-04-13 12:26:29 -0400

    Tailwind css setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js, jsx, tsx, ts}",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    letterSpacing: {
+      tight: '-.015em'
+    },
+    extend: {
+      height: {
+        'half-screen': '50vh'
+      },
+      animation: {
+        'fadeIn-1': 'fade 1s linear',
+        'fadeIn-3': 'fade 3s linear',
+      },
+      keyframes: {
+        fade: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 100 },
+        }
+      }
+    },
   },
   plugins: [],
 }
